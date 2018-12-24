945b05e8be33050bf4494fb55863a6d602978f43
srebe <srebe@d18dce58-7b58-a040-b2ea-01fdf0481e3c>
2018-12-24 23:36:23 +0000

    boost 1_69_0 llvm 8.0
    git-svn-id: https://paps-pc/svn/dISToBJ/trunk@12 d18dce58-7b58-a040-b2ea-01fdf0481e3c

--- Test/BaseTests.cpp
+++ Test/BaseTests.cpp
@@ -6,6 +6,7 @@
 #include <iostream>
 #include <chrono>
 #include <thread>
+#include <boost\date_time.hpp>
 //---------------------------------------------------------------------------
 #pragma package(smart_init)
 using namespace srdev;
@@ -17,6 +18,18 @@ bool test = Test::addTestCase(make_shared<Test::BaseTests>(), "BaseTests");
 
 srdev::TestResult srdev::Test::BaseTests::runTest()
 {
-	std::this_thread::sleep_for(std::chrono::seconds(2));
+	std::this_thread::sleep_for(std::chrono::seconds(2));	using namespace boost::gregorian;
+
+	date today = day_clock::local_day();
+	partial_date new_years_day(1, Jan);
+	//Subtract two dates to get a duration
+	days days_since_year_start = today - new_years_day.get_date(today.year());
+	std::cout << "Days since Jan 1: " << days_since_year_start.days()
+			  << std::endl;
+
+	days days_until_year_start = new_years_day.get_date(today.year() + 1) - today;
+	std::cout << "Days until next Jan 1: " << days_until_year_start.days()
+			  << std::endl;
+
 	return TestResult::Successful;
 }
